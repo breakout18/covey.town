a84b37a21ee8b8b3ee7f2220380091e9252aa908
kamira264 <camicat123@gmail.com>
2021-04-13 19:53:05 -0500

    message ui 2

--- frontend/src/classes/Player.ts
+++ frontend/src/classes/Player.ts
@@ -1,3 +1,5 @@
+export type ChatMessage = { id: string; sender: ServerPlayer; message: string; timestamp: number}
+
 export default class Player {
   public location?: UserLocation;
 
@@ -8,6 +10,10 @@ export default class Player {
   public sprite?: Phaser.GameObjects.Sprite;
 
   public label?: Phaser.GameObjects.Text;
+
+  public message?: Phaser.GameObjects.Text;
+
+  public chatMessage?: ChatMessage;
 
   constructor(id: string, userName: string, location: UserLocation) {
     this._id = id;
@@ -22,6 +28,12 @@ export default class Player {
   get id(): string {
     return this._id;
   }
+
+  removeMessage() : void {
+    this.chatMessage = undefined;
+    this.message?.destroy();
+  }
+
 
   static fromServerPlayer(playerFromServer: ServerPlayer): Player {
     return new Player(playerFromServer._id, playerFromServer._userName, playerFromServer.location);
